86d1a50eaed258b13ae138f82d7154ea50ba1d73
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2026-02-19 05:33:06 +0700

    fix: variable env name

--- app/auth.ts
+++ app/auth.ts
@@ -59,7 +59,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   session: {
     strategy: "jwt",
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXT_AUTH_SECRET,
 });
 
 export { handlers as GET, handlers as POST };
